15fd194a1c35d4b11913c6a99a78769ad1715bc9
Ahmed Khalaf <59212055+Ahmedkhalaf477@users.noreply.github.com>
2021-10-20 00:13:42 +0200

    Delete Iphost.py

--- Iphost.py
+++
@@ -1,8 +0,0 @@
-import socket 
-####################
-print( " >>>>>>>>>>>>>>>> ELmagic Ahmed_Khalaf <<<<<<<<<<<<<< ")
-print( " >>>>>>>>>> Wolcome To Script Ex_Ip Host <<<<<<<<<<<<< ")
-host = input (" Enter Your Hostname   : " )
-serv=socket.gethostbyname(host)
-
-print(serv)
